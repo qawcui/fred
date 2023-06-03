0e91e96b6308d5a2c782cc5d6eb716e2b6de3889
qawcui <135426874+qawcui@users.noreply.github.com>
2023-06-03 06:55:06 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # 定义 UUID 及 伪装路径,请自行修改.(注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
-UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
-VMESS_WSPATH=${VMESS_WSPATH:-'/vmess'}
-VLESS_WSPATH=${VLESS_WSPATH:-'/vless'}
+UUID=${UUID:-'0bd85246-f78e-4b13-8bf1-9863895d0322'}
+VMESS_WSPATH=${VMESS_WSPATH:-'/0bd85246-f78e-4b13-8bf1-9863895d0322-vmess'}
+VLESS_WSPATH=${VLESS_WSPATH:-'/0bd85246-f78e-4b13-8bf1-9863895d0322-vless'}
 TROJAN_WSPATH=${TROJAN_WSPATH:-'/trojan'}
 SS_WSPATH=${SS_WSPATH:-'/shadowsocks'}
 sed -i "s#UUID#$UUID#g;s#VMESS_WSPATH#${VMESS_WSPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g;s#TROJAN_WSPATH#${TROJAN_WSPATH}#g;s#SS_WSPATH#${SS_WSPATH}#g" config.json
